7e08fb3819d12ba7cc084a334a6bb7b1965725f2
Md. Zeeshan Ghaniwala <ghaniwalamohmmad@gmail.com>
2024-03-11 18:31:42 +0530

    init branch

--- app.js
+++ app.js
@@ -1,54 +1,94 @@
-let num = Math.floor(Math.random() * 11);
 
-console.log(num)
+$('#loc-input').keypress(function (e) {
 
-let hint = $('#hint')
-hint.text('Game Started');
-
-let no_guesses = 10;
-
-$("#inputfield").on('keypress', function (e) {
-  
   if (e.which === 13) {
-    takeGuess();
-   }
-});
 
-$("#inputbtn").on('click', function () {
-  takeGuess()
-});
-
-function takeGuess() {
-  if(isNaN($('#inputfield').val())) {
-    hint.text('Guess should be number');
-    return;
-  } 
-
-  $(document.body).removeClass('large')
-  $(document.body).removeClass('small')
-  
-  if(no_guesses <= 0){
-    $('#num').text(num);
-    hint.text('Game Over');
-    return;
+    fetchData($('#loc-input').val())
+      .then(data => {
+        displayWeatherData(data);
+        saveSearchData(data.location.name);
+      });
+    $('#loc-input').val('')
+    $('#on-error').text('');
   }
-  console.log($('#inputfield').val());
-  
-  let numval = $('#inputfield').val()
+});
+const fetchData = async (location) => {
+  try {
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=6560c19d22b041d5b1c34607241103&q="
+      + location + "&aqi=no", {
+      method: 'GET',
+    });
 
-  if (numval == num) {
-    $('#num').text(num);
-    hint.text('You guessed right');
-    no_guesses = 0
-  } else if (numval > num) {
-    hint.text('Try Smaller Number');
-    $(document.body).addClass('large')
-    no_guesses -= 1 
-  } else if (numval < num) {
-    hint.text('Try Larger Number');
-    no_guesses -= 1 
-    $(document.body).addClass('small')
-  } 
-  $('#inputfield').val("")
-  $('#gleft').text(no_guesses);
+    if (!response.ok) {
+      throw new Error("Try to Spell Correct City");
+    }
+
+    const json = await response.json();
+
+
+    return json;
+
+  } catch (err) {
+    $('#on-error').text(err);
+    console.error(err);
+  }
+
 }
+
+const displayWeatherData = (json) => {
+
+  $('#weather-icon').attr("src", `https:${json.current.condition.icon}`);
+
+  $('#location').text(json.location.name);
+
+  $(".details").html(`
+  <h3>${json.current.temp_c}°C</h3>
+  <p>${json.current.condition.text}</p>
+  <p>Humidity: ${json.current.humidity}%</p>
+  <p>Wind: ${json.current.wind_kph} KPH</p>
+  `)
+}
+
+const saveSearchData = (search) => {
+  let data = JSON.parse(localStorage.getItem('SearchData'));
+  if (!Array.isArray(data)) {
+    data = []
+  }
+  data.push(search)
+  localStorage.setItem('SearchData', JSON.stringify(data));
+
+  $('#recent').prepend("<li> " + search + " </li>");
+}
+
+const displaySearchData = () => {
+  const storedData = JSON.parse(localStorage.getItem('SearchData'));
+  storedData.forEach((element) => {
+    $('#recent').prepend("<li> " + element + " </li>")
+  })
+
+  var lis = document.getElementById('recent')
+    .getElementsByTagName('li');
+  for (i = 0; i < lis.length; i++) {
+   
+    lis[i].onclick = finalFunc;
+  }
+  console.log(storedData)
+
+}
+
+function finalFunc() {
+const loca = this.innerHTML 
+console.log(this)
+  fetchData(loca)
+      .then(data => {
+        displayWeatherData(data);
+        saveSearchData(data.location.name);
+      });
+    $('#loc-input').val(loca)
+    $('#on-error').text('');
+}
+
+function clearRecentSearch() {
+  localStorage.removeItem("SearchData");
+}
+displaySearchData()
